02f87d854705f7f85c59a0872ffa5b2697dbcd09
araxis <oreshko2010@gmail.com>
2018-09-01 03:34:00 +0300

    init

---
+++ DatingApp.API/DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Value> Values { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
