4729fab47723f8c78c98fab09c67ee92cf5b57d3
araxis <oreshko2010@gmail.com>
2018-09-05 02:32:22 +0300

    user add fields

---
+++ DatingApp.API/DatingApp.API/Data/Seed.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace DatingApp.API.Data
+{
+    public class Seed
+    {
+        private readonly DataContext _context;
+
+        public Seed(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void SeedUsers()
+        {
+            var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+            //var users = JsonConvert
+        }
+    }
+}
